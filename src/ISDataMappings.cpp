47bf70cee9b360ddf29da1c5bff57037efb32715
Andrew Priddis <andrew.priddis@inertialsense.com>
2022-04-07 13:42:30 -0600

    ErrorCode added to SKD

--- src/ISDataMappings.cpp
+++ src/ISDataMappings.cpp
@@ -1198,6 +1198,7 @@ static void PopulateEvbLunaWheelControllerMappings(map_name_to_info_t mappings[D
 	ADD_MAP(m, totalSize, "effInt_r", effInt_r, 0, DataTypeFloat, float, 0);
 	ADD_MAP(m, totalSize, "effDuty_l", effDuty_l, 0, DataTypeFloat, float, 0);
 	ADD_MAP(m, totalSize, "effDuty_r", effDuty_r, 0, DataTypeFloat, float, 0);
+	ADD_MAP(m, totalSize, "errorCode", errorCode, 0, DataTypeInt32, int32_t, 0);
 
 	ASSERT_SIZE(totalSize);
 }
